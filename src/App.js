f5760776077fd113a3d226ce95f4940bd96c2216
marb61a <marb61a@gmail.com>
2019-01-09 21:27:42 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { recipes } from "./tempList";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.getRecipes = this.getRecipes.bind(this);
+  }
+
+  state = {
+    recipes: recipes
+  }
+
+  async getRecipes() {
+    try {
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = () => {
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <React.Fragment>{this.displayPage()}</React.Fragment>;
   }
 }
 
