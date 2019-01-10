15ad1b92d7cfe8ce901d4586d79e81fadc1d8f43
marb61a <marb61a@gmail.com>
2019-01-10 16:41:23 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 import { recipes } from "./tempList";
 class App extends Component {
   constructor(props) {
@@ -10,12 +12,24 @@ class App extends Component {
   }
 
   state = {
-    recipes: recipes
-  }
+    recipes: recipes,
+    base_url: "",
+    url: "",
+    pageIndex: 1,
+    details_id: 35382,
+    search: "",
+    query: "&q="
+  };
 
   async getRecipes() {
     try {
+      console.log(this.state.url);
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
 
+      this.setState({
+        recipes: jsonData.recipes
+      });
     } catch (error) {
       console.log(error);
     }
@@ -26,7 +40,20 @@ class App extends Component {
   }
 
   displayPage = () => {
-
+    switch(this.state.pageIndex) {
+      default:
+      case 1 :
+        return (
+          <RecipeList 
+            handleDetails={this.handleDetails}
+            recipes={this.state.recipes}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            search={this.state.search}
+          />
+        )
+      case 0 :
+    }
   }
 
   render() {
