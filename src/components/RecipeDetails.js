2b3e284f375ae0d43a990688829715955790c2bf
marb61a <marb61a@gmail.com>
2019-01-10 21:08:15 +0000

    updated RecipeDetails component

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -6,15 +6,42 @@ export default class RecipeDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      recipe: recipe
+      recipe: recipe,
+      url: ``
+    };
+  };
+
+  async componentDidMount() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipe: jsonData.recipe
+      });
+    } catch(error) {
+      console.log(error);
     }
-  }
+  };
 
   render() {
+    const {
+      image_url,
+      publisher,
+      publisher_url,
+      source_url,
+      title,
+      ingredients
+    } = this.state.recipe;
+    const { handleIndex } = this.props;
+
     return (
       <div className="container my-5">
         <div className="row">
-        
+          <div className="col-10 mx-auto col-md-6 my-3">
+            <button>
+              back to recipe list
+            </button>
+          </div>
         </div>
       </div>
     );
