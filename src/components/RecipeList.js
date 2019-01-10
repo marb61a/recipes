184d0167fff653a2e4b8929cba836f7d0d1c7625
marb61a <marb61a@gmail.com>
2019-01-10 12:17:08 +0000

    Updated RecipeList

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -10,7 +10,34 @@ export default class RecipeList extends Component {
 
     return (
       <React.Fragment>
-      
+        <RecipeSearch 
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          search={search}
+        />
+        <div className="container my-5">
+          {/* Title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">
+                Recipe List
+              </h1>
+            </div>
+          </div>
+          {/* End Title*/}
+          <div className="row">
+            {recipes.map(recipe => {
+              return (
+                <Recipe 
+                  key={recipe.recipe_id}
+                  recipe={recipe}
+                  handleDetails={handleDetails}
+                  search={search}
+                />
+              )
+            })}
+          </div>
+        </div>
       </React.Fragment>
     )
   }
